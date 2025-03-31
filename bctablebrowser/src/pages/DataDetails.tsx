3b1c3de6e1f2b3b3dac527df7c7ad450b98e6eab
Jacek Jastrzembski <jacekj22@wp.pl>
2025-03-31 17:39:12 +0200

    Stworzenie podstrony ze szczegółami

---
+++ bctablebrowser/src/pages/DataDetails.tsx
@@ -0,0 +1,52 @@
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Box, Checkbox, Stack, Typography } from '@mui/material';
+import { useParams } from 'react-router-dom';
+import data from '../assets/data.json';
+
+interface Table {
+  name: string;
+  columns: Array<{ name: string; type: string; isSynced?: boolean }>;
+}
+
+interface RouteParams extends Record<string, string | undefined> {
+  tableName?: string;
+}
+
+export default function DataDetails () {
+  const { tableName } = useParams<RouteParams>();
+  const table = data.find((t: Table) => t.name === tableName);
+
+  if (!table) {
+    return <Typography color='red'>Tabela nie znaleziona</Typography>;
+  }
+
+  const columns: GridColDef[] = [
+    { field: 'name', headerName: 'Nazwa kolumny', width: 280, minWidth: 150 },
+    { field: 'type', headerName: 'Typ', width: 250, minWidth: 150 },
+    {
+      field: 'isSynced',
+      headerName: 'Synchronizowana?',
+      width: 200,
+      renderCell: (params) => (
+        <Checkbox checked={params.value} readOnly />
+      ),
+    },
+  ];
+
+    return (
+            <Box sx={{ width: '100%' }}>
+                <Typography color='black' marginBottom={'1rem'} variant="h5">Szczegóły tabeli: {tableName}</Typography>
+                <Stack direction="row" spacing={1} sx={{ mb: 1 }}>   
+                    <div style={{ width: '100%', maxHeight: 800 }}>
+                        <DataGrid
+                            rows={table.columns}
+                            columns={columns}
+                            //   paginationModel={{ page: 0, pageSize: 11 }}
+                            getRowId={(row) => row.name}
+                            checkboxSelection={true}
+                        />
+                    </div>
+                </Stack>
+            </Box>
+    );
+};
